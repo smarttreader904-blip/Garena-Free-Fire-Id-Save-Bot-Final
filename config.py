503425471576ba2a213494a9c6f0df2c85b01fbf
smarttreader904-blip <smarttreader904@gmail.com>
2026-06-11 18:14:32 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,23 @@
-# config.py
-# FF Bot template file
+# Bot Token
+BOT_TOKEN = "8584899858:AAG0ZDQGKHbsuwwYFSn8HdpHAXitNfyMpOM"
+
+# Admin IDs
+ADMIN_IDS = [
+    8559227368,
+    6187018016
+]
+
+# Database
+DATABASE_NAME = "ff_bot.db"
+
+# Multi Admin
+MULTI_ADMIN = True
+
+# Duplicate UID Check
+ALLOW_DUPLICATE_UID = False
+
+# Categories
+CATEGORIES = [
+    "Heroic",
+    "Grandmaster"
+]
